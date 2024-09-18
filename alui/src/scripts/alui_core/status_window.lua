980bea27f000bfe1fb8e9d3175a440dc1831a92e
Brett <Brett@WIPDeveloper.com>
2024-09-18 10:21:06 -0500

    chance status window to use cecho instead of echo

--- alui/src/scripts/alui_core/status_window.lua
+++ alui/src/scripts/alui_core/status_window.lua
@@ -1,16 +1,24 @@
 function status_window(e)
-  if e ~= "alui status window" then return end
+    if e ~= "alui status window" then
+        return
+    end
 
-  alui.combatmini:clear()
-  local status = alui.status
-  if status.hunger then alui.combatmini:echo(f"You are {status.hunger} and {status.thirst}.\n") end
-  if status.fatigue then alui.combatmini:echo(f"You are {status.fatigue}.\n") end
-  if status.posture then alui.combatmini:echo(f"You are {status.posture}.\n") end
-  
-  local m = alui.combatmini
-  
-  if alui.health then
-    m:echo(f[[
+    alui.combatmini:clear()
+    local status = alui.status
+    if status.hunger then
+        alui.combatmini:cecho(f "You are {status.hunger} and {status.thirst}.\n")
+    end
+    if status.fatigue then
+        alui.combatmini:cecho(f "You are {status.fatigue}.\n")
+    end
+    if status.posture then
+        alui.combatmini:cecho(f "You are {status.posture}.\n")
+    end
+
+    local m = alui.combatmini
+
+    if alui.health then
+        m:cecho(f [[
 
       Head: {alui.health.head}/9
       Body: {alui.health.body}/9
@@ -18,20 +26,20 @@ function status_window(e)
  Right Arm: {alui.health["right arm"]}/9
   Left Leg: {alui.health["left leg"]}/9
  Right Leg: {alui.health["right leg"]}/9]])
-  end
-  if alui.health["left wing"] then 
-    m:echo(f[[
+    end
+    if alui.health["left wing"] then
+        m:cecho(f [[
 
  Left Wing: {alui.health["left wing"]}/9]])
-  end
-  if alui.health["right wing"] then
-    m:echo(f[[
+    end
+    if alui.health["right wing"] then
+        m:cecho(f [[
 
 Right Wing: {alui.health["right wing"]}/9]])
-  end
-  if alui.health.tail then
-    m:echo(f[[
+    end
+    if alui.health.tail then
+        m:cecho(f [[
 
       Tail: {alui.health.tail}/9]])
-  end
+    end
 end
