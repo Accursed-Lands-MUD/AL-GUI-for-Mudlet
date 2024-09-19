6306deb25079af07d9adb20e1ca76df8b63d02ad
Brett <Brett@WIPDeveloper.com>
2024-09-18 19:26:20 -0500

    change limb order

--- alui/src/scripts/alui_core/status_window.lua
+++ alui/src/scripts/alui_core/status_window.lua
@@ -20,23 +20,30 @@ function status_window(e)
     if alui.health then
         m:cecho(f [[
 
-      Head: {alui.health.head}/9
       Body: {alui.health.body}/9
+      Head: {alui.health.head}/9
   Left Arm: {alui.health["left arm"]}/9
- Right Arm: {alui.health["right arm"]}/9
-  Left Leg: {alui.health["left leg"]}/9
- Right Leg: {alui.health["right leg"]}/9]])
-    end
-    if alui.health["left wing"] then
-        m:cecho(f [[
+  Left Leg: {alui.health["left leg"]}/9]])
+        if alui.health["left wing"] then
+            m:cecho(f [[
 
  Left Wing: {alui.health["left wing"]}/9]])
-    end
-    if alui.health["right wing"] then
+        end
+
         m:cecho(f [[
 
+ Right Arm: {alui.health["right arm"]}/9
+ Right Leg: {alui.health["right leg"]}/9]])
+
+        if alui.health["right wing"] then
+            m:cecho(f [[
+
 Right Wing: {alui.health["right wing"]}/9]])
+        end
+
+
     end
+
     if alui.health.tail then
         m:cecho(f [[
 
