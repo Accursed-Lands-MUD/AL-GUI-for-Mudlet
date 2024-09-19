57b5a1025daf3411f857cfa882f3afa6b2d82797
Brett <Brett@WIPDeveloper.com>
2024-09-18 19:26:09 -0500

    streamline colored message

--- alui/src/scripts/alui_core/status_update.lua
+++ alui/src/scripts/alui_core/status_update.lua
@@ -1,18 +1,18 @@
 alui.status = alui.status or {}
 
 local fatigue_levels = {
-    ["well rested"] = "<ansi_cyan>",
-    ["barely tired"] = "<ansi_green>",
-    ["somewhat tired"] = "<ansi_green>",
-    ["winded"] = "<ansi_green>",
-    ["tired"] = "<ansi_green>",
-    ["weary"] = "<ansi_light_yellow>",
-    ["haggard"] = "<ansi_light_yellow>",
-    ["worn out"] = "<ansi_light_yellow>",
-    ["exhausted"] = "<ansi_light_yellow>",
-    ["disoriented"] = "<ansi_light_red>",
-    ["faint"] = "<ansi_light_red>",
-    ["system shocked"] = "<ansi_light_red>"
+    ["well rested"] = "<ansi_cyan>well rested<reset>",
+    ["barely tired"] = "<ansi_green>barely tired<reset>",
+    ["somewhat tired"] = "<ansi_green>somewhat tired<reset>",
+    ["winded"] = "<ansi_green>winded<reset>",
+    ["tired"] = "<ansi_green>tired<reset>",
+    ["weary"] = "<ansi_light_yellow>weary<reset>",
+    ["haggard"] = "<ansi_light_yellow>haggard<reset>",
+    ["worn out"] = "<ansi_light_yellow>worn out<reset>",
+    ["exhausted"] = "<ansi_light_yellow>exhausted<reset>",
+    ["disoriented"] = "<ansi_light_red>disoriented<reset>",
+    ["faint"] = "<ansi_light_red>faint<reset>",
+    ["system shocked"] = "<ansi_light_red>system shocked<reset>"
 }
 
 function status_update(e)
@@ -22,17 +22,9 @@ function status_update(e)
 
     local status = gmcp.Char.Status
 
-    alui.status.fatigue = set_fatigue_color(status.Fatigue)
+    alui.status.fatigue = fatigue_levels[status.Fatigue]
     alui.status.posture = status.Posture
     alui.status.meline = f "You are {status.Name:title()}, a {status.Age} year old {status.Race}."
 
     raiseEvent("alui status window")
-end
-
-function set_fatigue_color(level)
-    -- check fatigue level against table and set color accordingly
-
-    local fatigue_color = fatigue_levels[level]
-    
-    return fatigue_color .. level .. "<reset>"
 end
--- alui/src/scripts/alui_core/vitals_update.lua
+++ alui/src/scripts/alui_core/vitals_update.lua
@@ -2,56 +2,44 @@ alui.status = alui.status or {}
 alui.health = alui.health or {}
 
 local thirst_colors = {
-    ["bloated"] = "<ansi_green>",
-    ["quenched"] = "<ansi_green>",
-    ["not thirsty"] = "<ansi_green>",
-    ["slightly thirsty"] = "<yellow>",
-    ["moderately thirsty"] = "<yellow>",
-    ["thirsty"] = "<yellow>",
-    ["very thirsty"] = "<red>",
-    ["parched"] = "<red>",
-    ["dehydrated"] = "<red>",
-    ["dying of thirst"] = "<red>",
+    ["bloated"] = "<ansi_green>bloated<reset>",
+    ["quenched"] = "<ansi_green>quenched<reset>",
+    ["not thirsty"] = "<ansi_green>not thirsty<reset>",
+    ["slightly thirsty"] = "<yellow>slightly thirsty<reset>",
+    ["moderately thirsty"] = "<yellow>moderately thirsty<reset>",
+    ["thirsty"] = "<yellow>thirsty<reset>",
+    ["very thirsty"] = "<ansi_red>very thirsty<reset>",
+    ["parched"] = "<ansi_red>parched<reset>",
+    ["dehydrated"] = "<ansi_light_red>dehydrated<reset>",
+    ["dying of thirst"] = "<ansi_light_red>dying of thirst<reset>",
 }
 
 local hunger_colors = {
-    ["stuffed"] = "<ansi_green>",
-    ["full"] = "<ansi_green>",
-    ["satiated"] = "<ansi_green>",
-    ["not hungry"] = "<ansi_green>",
-    ["peckish"] = "<yellow>",
-    ["slightly hungry"] = "<yellow>",
-    ["very hungry"] = "<red>",
-    ["famished"] = "<red>",
-    ["starving"] = "<red>",
-    ["dying of hunger"] = "<red>",
+    ["stuffed"] = "<ansi_green>stuffed<reset>",
+    ["full"] = "<ansi_green>full<reset>",
+    ["satiated"] = "<ansi_green>satiated<reset>",
+    ["not hungry"] = "<ansi_green>not hungry<reset>",
+    ["peckish"] = "<yellow>peckish<reset>",
+    ["slightly hungry"] = "<yellow>sightly hungry<reset>",
+    ["hungry"] = "<yellow>hungry<reset>",
+    ["very hungry"] = "<red>vry hungry<reset>",
+    ["famished"] = "<red>famished<reset>",
+    ["starving"] = "<red>starving<reset>",
+    ["dying of hunger"] = "<red>dying of hunger<reset>",
 }
 
 local health_levels = {
-    [0] = "<ansi_light_yellow:ansi_light_red>",
-    [1] = "<ansi_red:ansi_light_yellow>",
-    [2] = "<ansi_light_red>",
-    [3] = "<ansi_light_red>",
-    [4] = "<ansi_light_yellow>",
-    [5] = "<ansi_light_yellow>",
-    [6] = "<ansi_light_yellow>",
-    [7] = "<ansi_light_green>",
-    [8] = "<ansi_light_green>",
-    [9] = "<ansi_light_cyan>",
-}
-
-local health_to_number = {
-    ["destroyed"] = -1,
-    ["broken"] = 0,
-    ["massively injured"] = 1,
-    ["horribly injured"] = 2,
-    ["badly injured"] = 3,
-    ["injured"] = 4,
-    ["badly hurt"] = 5,
-    ["hurt"] = 6,
-    ["slightly wounded"] = 7,
-    ["barely wounded"] = 8,
-    ["in perfect health"] = 9
+    ["destroyed"] = "<ansi_light_yellow:ansi_light_red>-1<reset>",
+    ["broken"] = "<ansi_light_yellow:ansi_light_red>0<reset>",
+    ["massively injured"] = "<ansi_red:ansi_light_yellow>1<reset>",
+    ["horribly injured"] = "<ansi_light_red>2<reset>",
+    ["badly injured"] = "<ansi_light_red>3<reset>",
+    ["injured"] = "<ansi_light_yellow>4<reset>",
+    ["badly hurt"] = "<ansi_light_yellow>5<reset>",
+    ["hurt"] = "<ansi_light_yellow>6<reset>",
+    ["slightly wounded"] = "<ansi_light_green>7<reset>",
+    ["barely wounded"] = "<ansi_light_green>8<reset>",
+    ["in perfect health"] = "<ansi_light_cyan>9<reset>",
 }
 
 function vitals_update(e)
@@ -60,42 +48,15 @@ function vitals_update(e)
     end
     local vit = gmcp.Char.Vitals
 
-    alui.status.hunger = set_hunger_color(vit.Hunger)
-    alui.status.thirst = set_thirst_color(vit.Thirst)
+    alui.status.hunger = hunger_colors[vit.Hunger]
+    alui.status.thirst = thirst_colors[vit.Thirst]
 
     --handle healths
     if type(vit.List) == "table" then
         for part, health in pairs(vit.List) do
-            alui.health[part] = get_health_color_from_number(convert_health_to_number(health))
+            alui.health[part] = health_levels[health]
         end
     end
 
     raiseEvent("alui status window")
 end
-
-function set_thirst_color(level)
-    -- check thirst level against table and set color accordingly
-
-    local thirst_color = thirst_colors[level]
-    return thirst_color .. level .. "<reset>"
-end
-
-function set_hunger_color(level)
-    -- check hunger level against table and set color accordingly
-
-    local hunger_color = hunger_colors[level]
-    return hunger_color .. level .. "<reset>"
-end
-
-function convert_health_to_number(health)
-
-
-    return health_to_number[health] or 0
-end
-
-function get_health_color_from_number(healthLevel)
-
-
-    return health_levels[healthLevel] .. healthLevel .. "<reset>"
-end
-
