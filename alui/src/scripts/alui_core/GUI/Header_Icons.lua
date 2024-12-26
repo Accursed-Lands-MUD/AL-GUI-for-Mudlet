cd9dfd8456ea1c15b3e63f13055799e80ad87be5
Brett <Brett@WIPDeveloper.com>
2024-12-26 08:25:11 -0600

    add help images

--- alui/src/scripts/alui_core/GUI/Header_Icons.lua
+++ alui/src/scripts/alui_core/GUI/Header_Icons.lua
@@ -1,16 +1,14 @@
 local Package_Root = getMudletHomeDir()
 
-
 local blue = GUI.Colors.blue
 local red = GUI.Colors.red
 
-
 GUI.Header = Geyser.HBox:new({
-  name = "GUI.Header",
-  x = 0,
-  y = 0,
-  width = "100%",
-  height = "100%",
+    name = "GUI.Header",
+    x = 0,
+    y = 0,
+    width = "100%",
+    height = "100%",
 }, GUI.Top)
 
 GUI.IconCSS = CSSMan.new([[
@@ -32,73 +30,57 @@ GUI.IconCSS = CSSMan.new([[
 
 
 local function createMenuItem(name, updateFunction, parent)
-  local item = Geyser.Label:new({
-    name = 'GUI.Menu.' .. name,
-  }, parent)
+    local item = Geyser.Label:new({
+        name = 'GUI.Menu.' .. name,
+    }, parent)
 
+    item.update = updateFunction
 
-  item.update = updateFunction
+    item:update()
 
-  item:update()
+    -- setLabelToolTip(item.name, name)
 
-  -- setLabelToolTip(item.name, name)
-
-  return item
+    return item
 end
 
-
 local function createMenuButton(name, gameCommand, parent)
-  local button = Geyser.Button:new({
-    name = "GUI.Menu" .. name,
-    width = '100%',
-    tooltip = 'Toggle ' .. name,
+    local button = Geyser.Button:new({
+        name = "GUI.Menu" .. name,
+        width = '100%',
+        tooltip = 'Toggle ' .. name,
 
-    style = [[
+        style = [[
     margin: 5px;
     boarder-radius:5px;
     border: 1px solid white;
     ]],
 
-  }, parent)
-  return button
+    }, parent)
+    return button
 end
-
-
-
-
 
 GUI.Menu.Hunger = createMenuItem("Hunger", function(self)
     local iconPath = "url(" .. Package_Root .. "/alui/icons/medium/AL_hunger.png)"
     local label = nil
     local backgroundColor = alui.status.hunger
 
-
     if iconPath then
-      GUI.IconCSS:set("background-image", iconPath)
+        GUI.IconCSS:set("background-image", iconPath)
     else
-      GUI.IconCSS:set("background-image", "none")
-      if label then
-        self:echo("<center>" .. label)
-      end
+        GUI.IconCSS:set("background-image", "none")
+        if label then
+            self:echo("<center>" .. label)
+        end
     end
     if backgroundColor then
-      GUI.IconCSS:set("background-color", backgroundColor)
+        GUI.IconCSS:set("background-color", backgroundColor)
     else
-      GUI.IconCSS:set("background-color", "rgba(0,0,0,100)")
+        GUI.IconCSS:set("background-color", "rgba(0,0,0,100)")
     end
 
     self:setStyleSheet(GUI.IconCSS:getCSS())
-  end,
-  GUI.Header)
-
-
-
-
-
-
-
-
-
+end,
+        GUI.Header)
 
 GUI.Menu.Thirst = createMenuItem("Thirst", function(self)
     local iconPath = "url(" .. Package_Root .. "/alui/icons/medium/AL_thirst01.png)"
@@ -107,38 +89,31 @@ GUI.Menu.Thirst = createMenuItem("Thirst", function(self)
     GUI.IconCSS:set("background-image", iconPath)
 
     if backgroundColor then
-      GUI.IconCSS:set("background-color", backgroundColor)
+        GUI.IconCSS:set("background-color", backgroundColor)
     else
-      GUI.IconCSS:set("background-color", "rgba(0,0,0,100)")
+        GUI.IconCSS:set("background-color", "rgba(0,0,0,100)")
     end
 
     self:setStyleSheet(GUI.IconCSS:getCSS())
-  end,
-  GUI.Header)
-
-
-
-
-
+end,
+        GUI.Header)
 
 GUI.Menu.Fatigue = createMenuItem("Fatigue", function(self)
     local iconPath = "url(" .. Package_Root .. "/alui/icons/medium/AL_fatigue01.png)"
 
     local backgroundColor = alui.status.fatigue
 
-
     GUI.IconCSS:set("background-image", iconPath)
 
     if backgroundColor then
-      GUI.IconCSS:set("background-color", backgroundColor)
+        GUI.IconCSS:set("background-color", backgroundColor)
     else
-      GUI.IconCSS:set("background-color", "rgba(0,0,0,100)")
+        GUI.IconCSS:set("background-color", "rgba(0,0,0,100)")
     end
 
     self:setStyleSheet(GUI.IconCSS:getCSS())
-  end,
-  GUI.Header)
-
+end,
+        GUI.Header)
 
 GUI.Menu.Posture = createMenuItem("Posture", function(self)
     local label = alui.status.posture
@@ -146,26 +121,20 @@ GUI.Menu.Posture = createMenuItem("Posture", function(self)
     GUI.IconCSS:set("background-image", "none")
 
     if label then
-      self:clear()
-      self:echo("<center>" .. label)
+        self:clear()
+        self:echo("<center>" .. label)
     end
 
     GUI.IconCSS:set("background-color", "rgba(0,0,0,100)")
 
     self:setStyleSheet(GUI.IconCSS:getCSS())
-  end,
-  GUI.Header)
-
-
-
-
-
+end,
+        GUI.Header)
 
 GUI.Menu.Mercy = createMenuItem("Mercy", function(self)
     local iconPath = "url(" .. Package_Root .. "/alui/icons/medium/AL_mercy01.png)"
 
     local showMercy = alui.status.mercy
-
 
     GUI.IconCSS:set("background-image", iconPath)
 
@@ -173,108 +142,89 @@ GUI.Menu.Mercy = createMenuItem("Mercy", function(self)
     -- self:echo("<center> Mercy")
 
     if showMercy then
-      GUI.IconCSS:set("background-color", blue)
+        GUI.IconCSS:set("background-color", blue)
     else
-      GUI.IconCSS:set("background-color", red)
+        GUI.IconCSS:set("background-color", red)
     end
 
     self:setStyleSheet(GUI.IconCSS:getCSS())
-  end,
+end,
 
 
-
-  GUI.Header)
-
+        GUI.Header)
 
 GUI.Menu.Mercy:setClickCallback(function()
-  local gameCommand = "mercy on"
-  local toolTip = "Turn Mercy Off"
+    local gameCommand = "mercy on"
+    local toolTip = "Turn Mercy Off"
 
-  if alui.status.mercy then
-    gameCommand = "mercy off"
-    toolTip = "Turn Mercy On"
-  end
+    if alui.status.mercy then
+        gameCommand = "mercy off"
+        toolTip = "Turn Mercy On"
+    end
 
-  send(gameCommand, false)
-  setLabelToolTip("GUI.Menu.Mercy", toolTip)
+    send(gameCommand, false)
+    setLabelToolTip("GUI.Menu.Mercy", toolTip)
 end)
-
-
-
-
 
 GUI.Menu.Travel = createMenuItem("Travel", function(self)
     local iconPath = "url(" .. Package_Root .. "/alui/icons/medium/AL_travel01.png)"
 
     local autoTravel = alui.status.travel
 
-
     GUI.IconCSS:set("background-image", iconPath)
     -- self:echo("<center>Travel")
 
     if autoTravel then
-      GUI.IconCSS:set("background-color", blue)
+        GUI.IconCSS:set("background-color", blue)
     else
-      GUI.IconCSS:set("background-color", red)
+        GUI.IconCSS:set("background-color", red)
     end
 
     self:setStyleSheet(GUI.IconCSS:getCSS())
-  end,
-  GUI.Header)
-
-
-
+end,
+        GUI.Header)
 
 GUI.Menu.Travel:setClickCallback(function()
-  local gameCommand = "travel on"
-  local toolTip = "Turn Travel Off"
+    local gameCommand = "travel on"
+    local toolTip = "Turn Travel Off"
 
-  if alui.status.travel then
-    gameCommand = "travel off"
-    toolTip = "Turn Travel On"
-  end
+    if alui.status.travel then
+        gameCommand = "travel off"
+        toolTip = "Turn Travel On"
+    end
 
-  send(gameCommand, false)
-  setLabelToolTip("GUI.Menu.Travel", toolTip)
+    send(gameCommand, false)
+    setLabelToolTip("GUI.Menu.Travel", toolTip)
 end)
-
-
-
 
 GUI.Menu.CommonSense = createMenuItem("CommonSense", function(self)
     local iconPath = "url(" .. Package_Root .. "/alui/icons/medium/AL_commonsense.png)"
 
     local useCommonSense = alui.status.commonsense
 
-
     GUI.IconCSS:set("background-image", iconPath)
     -- self:echo("<center>CommonSense")
 
     if useCommonSense then
-      GUI.IconCSS:set("background-color", blue)
+        GUI.IconCSS:set("background-color", blue)
     else
-      GUI.IconCSS:set("background-color", red)
+        GUI.IconCSS:set("background-color", red)
     end
     self:setStyleSheet(GUI.IconCSS:getCSS())
-  end,
-  GUI.Header)
-
-
-
-
+end,
+        GUI.Header)
 
 GUI.Menu.CommonSense:setClickCallback(function()
-  local gameCommand = "commonsense on"
-  local toolTip = "Turn Commonsense Off"
+    local gameCommand = "commonsense on"
+    local toolTip = "Turn Commonsense Off"
 
+    if alui.status.commonsense then
+        gameCommand = "commonsense off"
+        toolTip = "Turn Commonsense On"
+    end
 
-  if alui.status.commonsense then
-    gameCommand = "commonsense off"
-    toolTip = "Turn Commonsense On"
-  end
-
-  send(gameCommand, false)
-  setLabelToolTip("GUI.Menu.CommonSense", toolTip)
+    send(gameCommand, false)
+    setLabelToolTip("GUI.Menu.CommonSense", toolTip)
 end)
 
 
@@ -293,17 +243,17 @@ end)
 
 
 
-GUI.Menu.Menu = createMenuItem("Menu", function(self)
-    local iconPath = "url(" .. Package_Root .. "/alui/icons/menuIcon.png)"
-
-
-
-
-    GUI.IconCSS:set("background-image", iconPath)
-
-    GUI.IconCSS:set("background-color", "rgba(0,0,0,100)")
-
-
-    self:setStyleSheet(GUI.IconCSS:getCSS())
-  end,
-  GUI.Header)
+--GUI.Menu.Menu = createMenuItem("Menu", function(self)
+--    local iconPath = "url(" .. Package_Root .. "/alui/icons/menuIcon.png)"
+--
+--
+--
+--
+--    GUI.IconCSS:set("background-image", iconPath)
+--
+--    GUI.IconCSS:set("background-color", "rgba(0,0,0,100)")
+--
+--
+--    self:setStyleSheet(GUI.IconCSS:getCSS())
+--  end,
+--  GUI.Header)
--- alui/src/scripts/alui_core/status_update.lua
+++ alui/src/scripts/alui_core/status_update.lua
@@ -1,27 +1,28 @@
-alui.status = alui.status or {}
-
-local blue = GUI.Colors.blue
-local green = GUI.Colors.green
-local yellow = GUI.Colors.yellow
-local orange = GUI.Colors.orange
-local red = GUI.Colors.red
-
-local fatigue_levels = {
-    ["well rested"] = blue,
-    ["barely tired"] = green,
-    ["somewhat tired"] = green,
-    ["winded"] = green,
-    ["tired"] = green,
-    ["weary"] = yellow,
-    ["haggard"] = yellow,
-    ["worn out"] = yellow,
-    ["exhausted"] = orange,
-    ["disoriented"] = orange,
-    ["faint"] = red,
-    ["system shocked"] = red
-}
-
 function status_update(e)
+
+    alui.status = alui.status or {}
+
+    local blue = GUI.Colors.blue
+    local green = GUI.Colors.green
+    local yellow = GUI.Colors.yellow
+    local orange = GUI.Colors.orange
+    local red = GUI.Colors.red
+
+    local fatigue_levels = {
+        ["well rested"] = blue,
+        ["barely tired"] = green,
+        ["somewhat tired"] = green,
+        ["winded"] = green,
+        ["tired"] = green,
+        ["weary"] = yellow,
+        ["haggard"] = yellow,
+        ["worn out"] = yellow,
+        ["exhausted"] = orange,
+        ["disoriented"] = orange,
+        ["faint"] = red,
+        ["system shocked"] = red
+    }
+
     if e ~= "gmcp.Char.Status" then
         return
     end
@@ -56,7 +57,7 @@ function status_update(e)
 
     if status.Name and status.Age and status.Race then
         alui.status.meline = "You are " ..
-            status.Name:title() .. ", a " .. status.Age .. " year old " .. status.Race .. "."
+                status.Name:title() .. ", a " .. status.Age .. " year old " .. status.Race .. "."
     end
     raiseEvent("alui status window")
 end
